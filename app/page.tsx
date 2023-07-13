217796753977ca5f66746f4ac9b53e4d179c189d
khaztaroth <astaroth503@gmail.com>
2023-07-13 18:17:09 -0400

    implemented overlay implemented svg handling implemented routes fixed filetree for proper routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 'use client'
-import { chatConnection } from "./chat/chatConnection";
-import { ShowMessages } from "./chat/chatMessages";
-
-chatConnection.connect()
 
 export default function Home() {
 
   return (
-    <div className="min-h-screen max-h-screen overscroll-y-contain mx-96 pb-10">
-      <ShowMessages/>
+    <div>
+      Default Page
     </div>
   )
 }
